37354fa06764c18b5bd42ba7eef6228792f39c5a
Renaldo Freire <renaldo@disroot.org>
2021-08-06 07:51:54 -0300

    imc.js updates

--- imc.js
+++ imc.js
@@ -6,5 +6,32 @@ function imc () {
     const peso = document.getElementById(peso).value;
     const resultado = document.getElementById('resultado');
 
-    if (nome !== '' && altura !== '' && peso !== '')
+    if (nome !== '' && altura !== '' && peso !== '') {
+
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let classificacao = '';
+
+        if (valorIMC < 18.5){
+            classificacao = 'abaixo do peso.';
+        }else if (valorIMC < 25){
+            classificacao = 'com peso ideal. Parabéns!';
+        }else if (valorIMC < 30){
+            classificacao = 'levemente acima do peso.';
+        }else if (valorIMC < 35){
+            classificacao = 'com obesidade grau I.';
+        }else if (valorIMC < 40){
+            classificacao = 'com obesiade grau II.';
+        }else {
+            classificacao = 'com obesidade grau III. Cuidado!';
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+
+    }else {
+        resultado.textContent = 'Por favor, preencha todos os campos!';
+    }
+
 }
+
+calcular.addEventListener('click',imc);
